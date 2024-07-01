ddd5cf355cb471da9fab3e445ff8905b60e7ce7c
het875 <hetglathiya875@gmail.com>
2024-07-01 15:48:44 +0530

    01-07-2024 frist

--- banking_system/authorisation/models.py
+++ banking_system/authorisation/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    updated = models.DateTimeField(auto_now=True, editable=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+
+class User(BaseModel) :
+    user_id = models.AutoField(primary_key=True)
+    last_name = models.CharField(max_length=13)
+    frist_name = models.CharField(max_length=13)
+    username = models.CharField(unique=True,db_index=True)
+    gender = models.CharField(
+        max_length=1, choices=(("m", "Male"), ("f", "Female")), null=False
+    )
+    date_of_birth = models.DateField()
+    email_id = models.EmailField(
+            max_length=10,
+            unique=True
+    )
+    mobile_number = models.CharField(
+        max_length=10
+    )
+    password = models.CharField(
+        max_length=100
+    )
+    is_mobile_verified = models.BooleanField(default=False)
+    is_email_verified = models.BooleanField(default=False)
+    is_blocked = models.BooleanField(default=False)
+    block_expiration = models.DateTimeField(null=True, blank=True)
+    last_login = models.DateTimeField(null=True, blank=True)
+    unsuccessful_attempts = models.IntegerField(default=0)
+    profile_picture = models.ImageField(
+        upload_to="profile_pictures/", null=True, blank=True
+    )
