79126e364ff6e014da80464afd2cd2c61e9994dd
het875 <hetglathiya875@gmail.com>
2024-07-01 15:52:55 +0530

    01-07-2024 frist

--- banking_system/authorisation/views.py
+++ banking_system/authorisation/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from pyexpat.errors import messages
+
+from django.conf import settings
+from django.contrib.auth import authenticate
+from django.contrib.auth.hashers import check_password
+
+from django.http import HttpResponseBadRequest, HttpResponse
+from django.shortcuts import redirect, render
+from django.utils import timezone
+from rest_framework import permissions
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import User
 
 # Create your views here.
+class UserListApiview(APIView):
+
+    Permission_classes =  [permissions.Allowany]
+
+    def submit_user_form (
+            request,
+            ):
+        if request.method == "POST":
+            username = request.POST.get("username")
+            if User.objects.filter(username=username).exists():
+                return render(request,"username allready exsist")
+            else:
+                frist_name = request.POST.get("frist_name")
+                last_name = request.POST.get("last_name")
+                username = request.POST.get("username")
+                email = request.POST.get("email")
